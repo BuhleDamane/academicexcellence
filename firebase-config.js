d9f38ec40bc97d11fec1b64a6ce753a9e981084a
Buhle Damane <145727499+BuhleDamane@users.noreply.github.com>
2026-02-22 21:36:33 +0200

    updated my firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-import { firebaseConfig } from './firebase-config.private.js';
+const firebaseConfig = {
+    apiKey: "AIzaSyB49ShVVKQmef4w68JLa9p0_KZVMMEzEYg",
+    authDomain: "academic-excellence-hub.firebaseapp.com",
+    projectId: "academic-excellence-hub",
+    storageBucket: "academic-excellence-hub.appspot.com",
+    messagingSenderId: "967792268188",
+    appId: "1:967792268188:web:7dfd9f4ab5757205ae2e59"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
---
+++ firebase-config.private.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB49ShVVKQmef4w68JLa9p0_KZVMMEzEYg",
+  authDomain: "academic-excellence-hub.firebaseapp.com",
+  projectId: "academic-excellence-hub",
+  storageBucket: "academic-excellence-hub.appspot.com",
+  messagingSenderId: "967792268188",
+  appId: "1:967792268188:web:7dfd9f4ab5757205ae2e59"
+};
